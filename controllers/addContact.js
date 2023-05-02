734426dc5fceb1d21f40ae3ac1e0734c78a10298
Vitalii89vn <vitalii.kuhta@gmail.com>
2023-05-02 21:58:58 +0300

    checked PUT and POST

--- controllers/addContact.js
+++ controllers/addContact.js
@@ -2,9 +2,8 @@ const contacts = require('../models/contacts');
 
 const addContact = async (req, res, next) => {
     try {
-        const result = contacts.addContact(req.body);
+        const result = await contacts.addContact(req.body);
         res.status(201).json(result);
-
     } catch (error) {
         next(error)
     }
--- middlewares/validateSchema.js
+++ middlewares/validateSchema.js
@@ -2,32 +2,17 @@ const {httpError} = require("../helpers");
 
 const validateBody = schema => {
     const validateFunc = (req, res, next) => {
-        // const err = schema.validate(body);
-        const { error} = schema.validate(req.body);
-        // const missedField = error.message.split(' ')[0].slice(1).slice(0,-1).toUpperCase();
-       
+        const { error } = schema.validate(req.body);
         if (!Object.keys(req.body).length) {
             throw httpError(400, "missing fields");
         };
         if (error) {
-            const missedField = error.message.split(' ')[0].slice(1).slice(0,-1).toUpperCase();
+            const missedField = error.details[0].context.key.toUpperCase();
             next(httpError(400, `missing required ${missedField} field`));
         }
         next()
     }
-
     return validateFunc;
 }
 
 module.exports = validateBody;
-
-// {\"value\":{\"name\":\"iii\"},
-// \"error\":{\"_original\":{\"name\":\"iii\"},
-// \"details\":
-// [
-// {
-// \"message\":\"\\\"email\\\" is required\",
-// \"path\":[\"email\"],
-// \"type\":\"any.required\",
-// \"context\":{\"label\":\"email\",\"key\":\"email\"}}]
-// }}
